fbff9027e33884ce8e49fc16999fe2583536c0e2
ErAbhi90 <er.abhithakral@gmail.com>
2019-05-14 10:30:56 +0530

    Initial Commit

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,20 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    MaterialApp(
+    MICard(),
+  );
+}
+
+class MICard extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
+        body: SafeArea(
+          child: Column(),
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
