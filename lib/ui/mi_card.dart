10388c0a673b0ca09bd1513c2f2e5461e1ab999a
ErAbhi90 <er.abhithakral@gmail.com>
2019-05-14 15:09:03 +0530

    Code Reformat

--- lib/ui/mi_card.dart
+++ lib/ui/mi_card.dart
@@ -1,45 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:mi_card/widgets/user_circular_image.dart';
-import 'package:mi_card/widgets/user_details_text.dart';
+import 'package:mi_card/widgets/card_details.dart';
 
 class MICard extends StatelessWidget {
-  final Widget _cardDetails = Column(
-    mainAxisAlignment: MainAxisAlignment.center,
-    children: <Widget>[
-      UserCircularImage(),
-      SizedBox(
-        height: 5.0,
-      ),
-      UserDetailsText(
-        userText: 'Abhishake Thakral',
-        userTextColor: Colors.white,
-        userFontSize: 40.0,
-        userFontFamily: 'Playball',
-      ),
-      SizedBox(
-        height: 5.0,
-      ),
-      UserDetailsText(
-        userText: '\"FLUTTER DEVELOPER\"',
-        userTextColor: Colors.white,
-        userFontSize: 16.0,
-        userFontFamily: 'Kodchasan',
-        userLetterSpacing: 3.0,
-        userFontWeight: FontWeight.bold,
-      ),
-      SizedBox(
-        height: 5.0,
-      ),
-      SizedBox(
-        height: 20.0,
-        width: 120.0,
-        child: Divider(
-          color: Colors.white,
-        ),
-      ),
-    ],
-  );
-
   @override
   Widget build(BuildContext context) {
     return SafeArea(
@@ -52,7 +14,7 @@ class MICard extends StatelessWidget {
               begin: Alignment.topLeft,
               end: Alignment.bottomRight),
         ),
-        child: _cardDetails,
+        child: CardDetails(),
       ),
     );
   }
---
+++ lib/widgets/card_details.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:mi_card/widgets/card/user_circular_image.dart';
+import 'package:mi_card/widgets/card/user_details_text.dart';
+
+class CardDetails extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        UserCircularImage(),
+        SizedBox(
+          height: 5.0,
+        ),
+        UserDetailsText(
+          userText: 'Abhishake Thakral',
+          userTextColor: Colors.white,
+          userFontSize: 40.0,
+          userFontFamily: 'Playball',
+        ),
+        SizedBox(
+          height: 5.0,
+        ),
+        UserDetailsText(
+          userText: '\"FLUTTER DEVELOPER\"',
+          userTextColor: Colors.white,
+          userFontSize: 16.0,
+          userFontFamily: 'Kodchasan',
+          userLetterSpacing: 3.0,
+          userFontWeight: FontWeight.bold,
+        ),
+        SizedBox(
+          height: 5.0,
+        ),
+        SizedBox(
+          height: 20.0,
+          width: 120.0,
+          child: Divider(
+            color: Colors.white,
+          ),
+        ),
+      ],
+    );
+  }
+}
